c863451bb562ecc33335ae9274d17252dc78d8f6
korneltomai <grizzlycentrum@gmail.com>
2023-03-02 22:08:52 +0100

    Add 'Solar Panels' page Implement adding new solar panels (currently not into the database) Fix heigth to height Rename 'testPage.razor' to 'TestPage.razor' beacause the program wouldn't build

--- Models/Napelem.cs
+++ Models/Napelem.cs
@@ -7,7 +7,10 @@ public class Napelem
 {
     [BsonRepresentation(BsonType.ObjectId)]
     public string id { get; } =ObjectId.GenerateNewId().ToString();
+    public string type { get; set; }
+    public string shape { get; set; }
     public int width { get; set; }
-    public int heigth { get; set; }
+    public int height { get; set; }
     public int output { get; set; }
+    public int price { get; set; }
 }
