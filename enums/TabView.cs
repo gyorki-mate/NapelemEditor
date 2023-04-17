a3b3c80b931313bba6fbb5d94d1a430171aa1708
gyorki.mate <73480462+gyorki-mate@users.noreply.github.com>
2023-04-17 12:54:44 +0200

    C l e a n (⌐■_■)

---
+++ enums/TabView.cs
@@ -0,0 +1,13 @@
+﻿namespace NapelemEditor;
+
+public class TabView
+{
+    public string Orientation { get; set; }
+    public string Name { get; set; }
+    public List<Objects> Obstacles = new();
+    public List<Objects> Panels = new();
+    public Guid Id { get; set; }
+    public bool CanvasLocked { get; set; }
+    public string Width { get; set; } = "1200";
+    public string Height { get; set; } = "500";
+}
