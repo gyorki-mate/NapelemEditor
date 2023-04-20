1f9c019f488e47765e78de6dc370382a69c4edbe
gyorki.mate <73480462+gyorki-mate@users.noreply.github.com>
2023-04-20 13:07:05 +0200

    Tabs render, cannot delete yet

--- enums/TabView.cs
+++ enums/TabView.cs
@@ -7,7 +7,10 @@ public class TabView
     public List<Objects> Obstacles = new();
     public List<Objects> Panels = new();
     public Guid Id { get; set; }
+
     public bool CanvasLocked { get; set; }
+
+    //Sizes are stored in ms
     public int Width { get; set; } = 10;
     public int Height { get; set; } = 10;
 }
