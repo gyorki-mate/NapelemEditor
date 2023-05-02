556c1a5fb94e23e3deb82502209f8ca0a536b318
gyorki.mate <73480462+gyorki-mate@users.noreply.github.com>
2023-05-02 13:43:53 +0200

    WIP draggable

--- enums/TabView.cs
+++ enums/TabView.cs
@@ -1,16 +1,15 @@
-﻿namespace NapelemEditor;
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NapelemEditor;
 
 public class TabView
 {
     public string Orientation { get; set; }
     public string Name { get; set; }
-    public List<Objects> Obstacles = new();
+    // public List<Objects> Obstacles = new();
     public List<Objects> Panels = new();
     public Guid Id { get; set; }
-
     public bool CanvasLocked { get; set; }
-
-    //Sizes are stored in ms
-    public int Width { get; set; } = 10;
-    public int Height { get; set; } = 10;
+    public string Width { get; set; } = "1200";
+    public string Height { get; set; } = "500";
 }
---
+++ wwwroot/JS/DragAndDrop.js
@@ -0,0 +1,53 @@
+﻿var snapObjects = [];
+snapObjects = document.getElementsByClassName("grid-item");
+
+function dragAndDrop(className) {
+    const position = {x: 0, y: 0}
+
+    interact(className).draggable({
+        listeners: {
+            start(event) {
+                console.log(event.type, event.target)
+            },
+            move(event) {
+                position.x += event.dx
+                position.y += event.dy
+
+                event.target.style.transform =
+                    `translate(${position.x}px, ${position.y}px)`
+            },
+        }, modifiers: [
+            interact.modifiers.restrictRect({
+                restriction: '.main',
+                endOnly: true
+            }),
+            interact.modifiers.snap({
+                targets: [interact.createSnapGrid({
+                    x: 50,
+                    y: 50,
+                })],
+                relativePoints: [{x: 0, y: 0}]
+            })]
+    })  // snap to the startCoords
+}
+
+var gridTarget = interact.snappers.grid({
+    // can be a pair of x and y, left and top,
+    // right and bottom, or width, and height
+    x: 50,
+    y: 50,
+
+    // optional
+    range: 10,
+
+    // optional
+    offset: {x: 5, y: 10},
+
+    // optional
+    limits: {
+        top: 0,
+        left: 0,
+        bottom: 500,
+        height: 500
+    }
+})
