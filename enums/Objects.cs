9fb5280cc406a103f08326f9a9e03ec20a2b90bc
gyorki.mate <73480462+gyorki-mate@users.noreply.github.com>
2023-05-12 17:02:00 +0200

    kinda done

--- enums/Objects.cs
+++ enums/Objects.cs
@@ -4,12 +4,14 @@ namespace NapelemEditor;
 
 public class Objects
 {
-    public Objects(double x, double y, int width, int height)
+    public Objects(double x, double y, int width, int height, int price=0, int output=0)
     {
         X = x;
         Y = y;
         Width = width;
         Height = height;
+        Price = price;
+        Output = output;
     }
 
     public double X { get; set; }
@@ -17,4 +19,6 @@ public class Objects
     public int Width { get; set; }
     public int Height { get; set; }
     public bool isLocked { get; set; } = false;
+    public int Price { get; set; }
+    public int Output { get; set; }
 }
--- enums/TabView.cs
+++ enums/TabView.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using NapelemEditor.Models;
 
 namespace NapelemEditor;
 
